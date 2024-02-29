9c9ef93dcd375c1a6a058fff3ff0d70c8bbb0b0a
PaJuS <pajus1337@gmail.com>
2024-02-29 07:14:09 +0100

    Add project files.

---
+++ Z2J-Hackaton/Hackaton_002_HastTable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hackaton_002_HastTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Z2J-Hackaton/Z2J-Hackaton/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Z2J_Hackaton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
